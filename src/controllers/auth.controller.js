4471ca6e071c65a53d18e570593441c0c77ebbe5
khamidullo <khkhamidullo@gmail.com>
2024-11-08 16:16:37 +0500

    auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,51 +1,62 @@
 import jwt from "jsonwebtoken";
-const users = new Map();
+import { User } from "../modules/index.js";
+import { statusCodes, errorMessages, ApiError } from "../utils/index.js";
 
-export const registerController = (req, res, next) => {
+export const registerController = async (req, res, next) => {
   try {
     const { email, role } = req.body;
-    const user = users.has(email);
+    const currentUser = await User.findOne({ email });
 
-    if (!user) {
-      if (!role) {
-        req.body.role = "user";
-      }
+    if (!currentUser) {
+      console.log({ currentUser });
+      const user = new User(req.body);
+      console.log({ user });
 
-      users.set(email, req.body);
-      return res.status(201).send("created");
+      await user.save();
+      return res.status(statusCodes.CREATED).send("created");
     }
-
-    return res.status(409).send("user already exists");
+    return res
+      .status(statusCodes.CONFLICT)
+      .send(errorMessages.EMAIL_ALREADY_EXISTS);
   } catch (error) {
-    next(error);
+    console.log(error)
+    next(new ApiError(error.statusCode, error.message));
   }
 };
 
-export const loginController = (req, res, next) => {
+export const loginController = async (req, res, next) => {
   try {
-    const { email } = req.body;
-    const user = users.has(email);
+    const { email, password } = req.body;
+    const currentUser = await User.find({ email });
 
-    if (!user) {
-      return res.status(404).send("Not found!");
+    if (!currentUser) {
+      return res
+        .status(statusCodes.NOT_FOUND)
+        .send(errorMessages.USER_NOT_FOUND);
     }
-    const userData = users.get(email);
+
+    const passwordIsEqual = User.compare(password);
+
+    if (!passwordIsEqual) {
+      return res
+        .status(statusCodes.BAD_REQUEST)
+        .send(errorMessages.INVALID_CREDENTIALS);
+    }
 
     const payload = {
       sub: email,
-      role: userData.role,
+      role: currentUser.role,
     };
 
-    const ttl = {
-      expiresIn: "20m",
-    };
+    const accessSecretKey = process.env.JWT_ACCESS_SECRET;
+    const refreshSecretKey = process.env.JWT_REFRESH_SECRET;
 
-    const accessSecretKey = "qwer12345";
-    const refreshSecretKey = "12345qwer";
+    const accessToken = jwt.sign(payload, accessSecretKey, {
+      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+    });
 
-    const accessToken = jwt.sign(payload, accessSecretKey, ttl);
     const refreshToken = jwt.sign(payload, refreshSecretKey, {
-      expiresIn: "7d",
+      expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
     });
 
     return res.send({
@@ -53,6 +64,6 @@ export const loginController = (req, res, next) => {
       refreshToken,
     });
   } catch (error) {
-    next(error);
+    next(new ApiError(error.statusCode, error.message));
   }
 };
---
+++ src/modules/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { hash, genSalt } from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "superAdmin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function () {
+  const salt = await genSalt(10);
+  const hashPassword = await hash(this.password, salt);
+  this.password = hashPassword;
+});
+
+
+userSchema.method("compare", function (userPassword) {
+  return bcrypt.compare(userPassword, this.password);
+});
+
+export const User = mongoose.model("user", userSchema);
